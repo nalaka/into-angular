876dbca0e9fcca4539d57acb1b0b96107722c295
Nalaka Jayasena <nalaka.jayasena@gmail.com>
2020-08-08 14:20:04 +0530

    Barebones AppComponent

--- projects/hello/src/app/app.component.spec.ts
+++ projects/hello/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hello'`, () => {
+  it(`should have as name 'world'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello');
+    expect(app.name).toEqual('world');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('hello app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Hello world!');
   });
 });
