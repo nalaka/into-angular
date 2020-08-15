4cb9166e0bb055641aafde03b8fbc614772a6825
Nalaka Jayasena <nalaka.jayasena@gmail.com>
2020-08-16 00:47:41 +0530

    Used ngFor and ngIf

--- projects/hello/src/app/foobar/foobar.component.ts
+++ projects/hello/src/app/foobar/foobar.component.ts
@@ -3,18 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-foobar',
   template: `
-    <p>
-      foobar works!
-    </p>
+    <ul>
+      <li *ngFor="let item of items">{{ item }}</li>
+    </ul>
+    <div *ngIf="selectedItem == ''; else tplNoItemSelected">
+      Please select an option
+    </div>
+    <ng-template #tplNoItemSelected>{{ selectedItem }}</ng-template>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class FoobarComponent implements OnInit {
+  items = ['foo', 'bar'];
+  selectedItem = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
