830a5ae60835d5c66cb62d6165e252df988b9c6f
Nalaka Jayasena <nalaka.jayasena@gmail.com>
2020-08-16 08:37:05 +0530

    Binding

--- projects/hello/src/app/foobar/foobar.component.ts
+++ projects/hello/src/app/foobar/foobar.component.ts
@@ -3,19 +3,52 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-foobar',
   template: `
-    <ul>
-      <li *ngFor="let item of items">{{ item }}</li>
-    </ul>
-    <div *ngIf="selectedItem == ''; else tplNoItemSelected">
-      Please select an option
+    <div>
+      <h3>Your options</h3>
+      <ul>
+        <li *ngFor="let item of items">{{ item }}</li>
+      </ul>
+
+      <div *ngIf="selectedItem == ''">
+        Please type-in one of the options.
+      </div>
+      <div *ngIf="items.includes(selectedItem)">
+        Selected option is: <strong>{{ selectedItem }}</strong>
+      </div>
+
+      <div>
+        <input
+          #txt
+          type="text"
+          [value]="selectedItem"
+          [attr.aria-label]="ariaLabel"
+          [class]="customClasses"
+          [style]="customStyles"
+        />
+        <button (click)="handleClick(txt.value)">Select</button>
+        <p *ngIf="msg != ''">{{ msg }}</p>
+      </div>
     </div>
-    <ng-template #tplNoItemSelected>{{ selectedItem }}</ng-template>
   `,
   styles: [],
 })
 export class FoobarComponent implements OnInit {
   items = ['foo', 'bar'];
   selectedItem = '';
+  msg = '';
+  ariaLabel = 'Option to select';
+  customClasses="class1 class2";
+  customStyles = 'width: 200px; background-color: lightblue';
+
+  handleClick(val: string) {
+    if (this.items.includes(val)) {
+      this.selectedItem = val;
+      this.msg = '';
+    } else {
+      this.selectedItem = '';
+      this.msg = 'Invalid option';
+    }
+  }
 
   constructor() {}
 
